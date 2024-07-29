7bf8aad4216bfda4a65388b6c25c862b1b9f454b
saitou-cpi <odeco_de_odeco@yahoo.co.jp>
2024-07-29 21:12:35 +0900

    add fileinfo.c

--- ls1.c
+++ ls1.c
@@ -3,36 +3,37 @@
 * 動作: 引数がない場合には、"."、そうでなければ引数のディレクトリ
 * に含まれるファイルを出力する
 */
-#include    <stdio.h>
-#include    <sys/types.h>
-#include    <dirent.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 void do_ls(char []);
 
-main(int ac, char *av[])
+int main(int ac, char *av[])  // ここを修正
 {
-    if ( ac == 1 )
-        do_ls( "." );
+    if (ac == 1)
+        do_ls(".");
     else
-        while ( --ac ){
-            printf("%s:\n", *++av );
-            do_ls( *av );
+        while (--ac) {
+            printf("%s:\n", *++av);
+            do_ls(*av);
         }
+    return 0;  // main関数の終わりに0を返す
 }
 
-void do_ls( char dirname[] )
+void do_ls(char dirname[])
 /*
 * dirnameというディレクトリのファイルをリストアップする
 */
 {
     DIR *dir_ptr;
-    struct dirent    *direntp;
+    struct dirent *direntp;
 
-    if ( ( dir_ptr = opendir( dirname ) ) == NULL )
-        fprintf(stderr,"ls1: cannot open %s\n", diname);
-    else{
-        while ( ( dirname = readdir( dir_ptr ) ) != NULL )
-            printf("%s\n", direntp->d_name );
-        closedir(dir_ptr)
+    if ((dir_ptr = opendir(dirname)) == NULL)
+        fprintf(stderr, "ls1: cannot open %s\n", dirname);
+    else {
+        while ((direntp = readdir(dir_ptr)) != NULL)  // ここを修正
+            printf("%s\n", direntp->d_name);
+        closedir(dir_ptr);
     }
 }
